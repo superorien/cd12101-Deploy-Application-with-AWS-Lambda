8e94ac5fbb41564ac9d5b9272f10fe883ba30c85
Oriental Tran <orientalinformation@gmail.com>
2023-10-28 16:22:37 +0700

    add URL

--- starter/backend/src/lambda/auth/auth0Authorizer.mjs
+++ starter/backend/src/lambda/auth/auth0Authorizer.mjs
@@ -4,7 +4,7 @@ import { createLogger } from '../../utils/logger.mjs'
 
 const logger = createLogger('auth')
 
-const jwksUrl = 'https://dev-kqitxjdiju47kmse.us.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-iwag7gdixu5hc26a.us.auth0.com/.well-known/jwks.json'
 
 export async function handler(event) {
   try {
